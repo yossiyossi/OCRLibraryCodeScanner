33df8a1a6c00c5366cdf2afacbeed26d17faf513
Yossi Hertzberg <yossi.hertzberg@gmail.com>
2018-03-05 15:05:17 -0800

    Added more pics and working start screens.

--- app/build/generated/source/r/debug/android/support/design/R.java
+++ app/build/generated/source/r/debug/android/support/design/R.java
@@ -639,11 +639,11 @@ public final class R {
         public static final int abc_textfield_search_default_mtrl_alpha = 0x7f07004d;
         public static final int abc_textfield_search_material = 0x7f07004e;
         public static final int abc_vector_test = 0x7f07004f;
-        public static final int design_fab_background = 0x7f07007a;
-        public static final int design_ic_visibility = 0x7f07007b;
-        public static final int design_snackbar_background = 0x7f07007c;
-        public static final int navigation_empty_icon = 0x7f070082;
-        public static final int notification_template_icon_bg = 0x7f070083;
+        public static final int design_fab_background = 0x7f07007b;
+        public static final int design_ic_visibility = 0x7f07007c;
+        public static final int design_snackbar_background = 0x7f07007d;
+        public static final int navigation_empty_icon = 0x7f070083;
+        public static final int notification_template_icon_bg = 0x7f070084;
     }
     public static final class id {
         public static final int action0 = 0x7f080000;
--- app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -496,7 +496,7 @@ public final class R {
         public static final int abc_textfield_search_default_mtrl_alpha = 0x7f07004d;
         public static final int abc_textfield_search_material = 0x7f07004e;
         public static final int abc_vector_test = 0x7f07004f;
-        public static final int notification_template_icon_bg = 0x7f070083;
+        public static final int notification_template_icon_bg = 0x7f070084;
     }
     public static final class id {
         public static final int action0 = 0x7f080000;
